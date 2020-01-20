ab09a43d8bb48e97114ecd222fdcab94b2ee5d94
amorCui <43329030+amorCui@users.noreply.github.com>
2020-01-21 02:30:39 +0800

    Update fireworker.js

--- resource/javascript/fireworker.js
+++ resource/javascript/fireworker.js
@@ -2,7 +2,22 @@
     'use strict';
 
     function FireWorkers(){
+        this.fireWorkers = [];
+        this.MAX_FIREWORKERS = 10;
         this._init_();
+
+        if(this.fireWorkers.length < this.MAX_FIREWORKERS){
+            var fireWorker = new FireWorker({
+                'canvas' : this.canvas
+            });
+            fireWorker.explosionColor = Math.floor(Math.random() * 360 / 10) * 10;
+            fireWorker.vel.y = Math.random() * -3 - 4;
+            fireWorker.vel.x = Math.random() * 6 - 3;
+            fireWorker.size = 8;
+            fireWorker.shrink = 0.999;
+            fireWorker.gravity = 0.01;
+            this.fireWorkers.push(fireWorker);
+        }
     }
     
     
@@ -12,24 +27,24 @@
         },
         _init_ : function(props){
             var _this = this;
-           
             var canvas = document.getElementById('fireworker');
             canvas.height = window.innerHeight;
             canvas.width = window.innerWidth;
-            _this.canvas = canvas;
-            _this.ctx = canvas.getContext("2d");
+            this.canvas = canvas;
+            this.ctx = canvas.getContext("2d");
 
-            _this._setOptions_(props);
+
+            this._setOptions_(props);
     
             addEventListener('resize',(e)=>{
                 canvas.height = e.target.innerHeight;
                 canvas.width = e.target.innerWidth;
-                _this.canvas = canvas;
-                _this._setOptions_();
+                this.canvas = canvas;
+                this._setOptions_();
             });
             addEventListener('click',(e)=>{
                 //添加一个指定位置爆炸的新的烟花
-                _this.add(e.x, e.y);
+                this.add(e.x, e.y);
             });
 
             function loop(){
@@ -42,15 +57,28 @@
     
         },
         render : function(ctx){
-            var _this = this;
             //清空
-            _this.clear();
-            //生成渐变色(一组烟花的颜色)
-            var fireWorker = new FireWorker({
-                'ctx' : _this.ctx,
-                'canvas' : _this.canvas,
-                'color' : Math.floor(Math.random() * 360 / 10) * 10
-            });
+            this.clear();
+            // //生成渐变色(一组烟花的颜色)
+            // var fireWorker = new FireWorker({
+            //     'ctx' : this.ctx,
+            //     'canvas' : this.canvas,
+            //     'color' : Math.floor(Math.random() * 360 / 10) * 10
+            // });
+
+            for(var fireWorker of this.fireWorkers){
+                fireWorker.update();
+                fireWorker.render(this.ctx);
+
+                // calculate distance with Pythagoras
+                var distance = Math.sqrt(Math.pow(this.canvas.width - fireWorker.pos.x, 2) + Math.pow(this.canvas.height - fireWorker.pos.y, 2));
+
+                // random chance of 1% if rockets is above the middle
+                var randomChance = fireWorker.pos.y < (this.canvas.width * 2 / 3) ? (Math.random() * 100 <= 1) : false;
+                if(fireWorker.pos.y < this.canvas.height / 5 || fireWorker.vel.y >= 0 || distance < 50 || randomChance){
+                    fireWorker.explode();
+                }
+            }
             
         },
         exist: function(){
@@ -60,12 +88,10 @@
     
         },
         clear : function(){
-            var _this = this;
-            _this.ctx.clearRect(0,0,_this.canvas.width,_this.canvas.height);  
+            this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);  
         },
         loop: function(){
-            var _this = this;
-            _this.render(_this.ctx);
+            this.render(this.ctx);
             
         }
         
@@ -74,22 +100,47 @@
 
 
     function FireWorker(props){
-        var _this = this;
-        _this.ctx = props.ctx;
-        _this.canvas = props.canvas;
+        // `var pos = new Position(this.canvas.width / 2, this.canvas.height);`
+        this.pos = props.pos ? props.pos: new Position(0,0);
+        this.Particles = [];
+        this.MAX_PARTICLES = 400;
+        this.canvas = props.canvas;
         //type 0 爆炸烟花, type 1 喷泉烟花
-        _this.type = props.type ? props.type: 0;
-        //烟花颜色
-        _this.color = props.color ? props.color: 0;
+        this.type = props.type ? props.type: 0;
+
+        //速度
+        this.vel = {
+            x: 0,
+            y: 0
+        };
+        //收缩速度
+        this.shrink = 0.97;
+        //阻力
+        this.resistance = 1;
+        //重力加速度
+        this.gravity = 0;
+
+        this.flick = false;
+
+        this.alpha = 1;
+        //淡出速度 - alpha
+        this.fade = 0;
+        //烟花粒子颜色
+        this.color = props.color ? props.color: 0;
+
+        //烟花Particle的数量
+        this.size = props.size? props.size: Math.floor(Math.random() * 5 + 5);
     }
 
     FireWorker.prototype = {
-        render : function(){
-            var pos = new Position(_this.canvas.width / 2, _this.canvas.height);
+        render : function(ctx){
+            var pos = new Position(this.canvas.width / 2, this.canvas.height);
             var p = new Particle({
                 'pos': pos,
-                'color' : _this.color
+                'canvas' : this.canvas ,
+                'color' : this.color
             });
+            p.size = 8;
             if(p.exist()){
                 p.render(ctx);
             }
@@ -103,7 +154,11 @@
         //喷泉烟花 0 上升阶段,            2 消失(释放)
         state : function(){
             return 0;
+        },
+        update : function(){
+
         }
+
 
     }
 
@@ -133,30 +188,52 @@
         this.flick = false;
 
         this.alpha = 1;
-        //褪色速度 - alpha
+        //淡出速度 - alpha
         this.fade = 0;
         //烟花粒子颜色
         this.color = props.color ? props.color: 0;
+
+        this.canvas = props.canvas;
     }
 
     Particle.prototype = {
         update : function(){
-            
+            // 施加阻力
+            this.vel.x *= this.resistance;
+            this.vel.y *= this.resistance;
+
+            // 施加重力
+            this.vel.y += this.gravity;
+
+            // 根据速度更新坐标
+            this.pos.x += this.vel.x;
+            this.pos.y += this.vel.y;
+
+            // 形状缩放
+            this.size *= this.shrink;
+
+            // 淡出
+            this.alpha -= this.fade;
         },
         render : function(ctx){
-            var _this = this;
             console.log('render Particle:');
-            var gradient = ctx.createRadialGradient(_this.pos.x, _this.pos.y, 0.1, _this.pos.x, _this.pos.y, _this.size / 2);
+            var gradient = ctx.createRadialGradient(this.pos.x, this.pos.y, 0.1, this.pos.x, this.pos.y, this.size / 2);
             gradient.addColorStop(0.1, "rgba(255,255,255," + this.alpha + ")");
-            gradient.addColorStop(0.8, "hsla(" + _this.color + ", 100%, 50%, " + _this.alpha + ")");
-            gradient.addColorStop(1, "hsla(" + _this.color + ", 100%, 50%, 0.1)");
+            gradient.addColorStop(0.8, "hsla(" + this.color + ", 100%, 50%, " + this.alpha + ")");
+            gradient.addColorStop(1, "hsla(" + this.color + ", 100%, 50%, 0.1)");
+
+            ctx.save();
+            //设置或返回如何将一个源（新的）图像绘制到目标（已有）的图像上。
+            //lighter: 显示源图像 + 目标图像。
+            ctx.globalCompositeOperation = 'lighter';
 
             ctx.fillStyle = gradient;
 
             ctx.beginPath();
-            ctx.arc(_this.pos.x, _this.pos.y, _this.flick ? Math.random() * _this.size : _this.size, 0, Math.PI * 2, true);
+            ctx.arc(this.pos.x, this.pos.y, this.flick ? Math.random() * this.size : this.size, 0, Math.PI * 2, true);
             ctx.closePath();
             ctx.fill();
+            ctx.restore();
         },
         //粒子消失的方法
         exist : function(){
