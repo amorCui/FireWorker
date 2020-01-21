2b7a4e31adcd49497b35bc9e9a9cf1d3caecd32d
amorCui <43329030+amorCui@users.noreply.github.com>
2020-01-21 17:45:39 +0800

    Update fireworker.js

--- resource/javascript/fireworker.js
+++ resource/javascript/fireworker.js
@@ -6,18 +6,7 @@
         this.MAX_FIREWORKERS = 10;
         this._init_();
 
-        if(this.fireWorkers.length < this.MAX_FIREWORKERS){
-            var fireWorker = new FireWorker({
-                'canvas' : this.canvas
-            });
-            fireWorker.explosionColor = Math.floor(Math.random() * 360 / 10) * 10;
-            fireWorker.vel.y = Math.random() * -3 - 4;
-            fireWorker.vel.x = Math.random() * 6 - 3;
-            fireWorker.size = 8;
-            fireWorker.shrink = 0.999;
-            fireWorker.gravity = 0.01;
-            this.fireWorkers.push(fireWorker);
-        }
+        setInterval(this.add, 800, this);
     }
     
     
@@ -44,7 +33,7 @@
             });
             addEventListener('click',(e)=>{
                 //添加一个指定位置爆炸的新的烟花
-                this.add(e.x, e.y);
+                this.add(this, e.x, e.y);
             });
 
             function loop(){
@@ -66,26 +55,49 @@
             //     'color' : Math.floor(Math.random() * 360 / 10) * 10
             // });
 
+            //状态是结束的fireworker要释放掉
+            this.fireWorkers = this.fireWorkers.filter(function(value, index, arr){
+                return value.state !== 2;
+            });
+
             for(var fireWorker of this.fireWorkers){
                 fireWorker.update();
                 fireWorker.render(this.ctx);
 
-                // calculate distance with Pythagoras
+                // 计算当前位置和最终位置之间的长度
                 var distance = Math.sqrt(Math.pow(this.canvas.width - fireWorker.pos.x, 2) + Math.pow(this.canvas.height - fireWorker.pos.y, 2));
 
                 // random chance of 1% if rockets is above the middle
                 var randomChance = fireWorker.pos.y < (this.canvas.width * 2 / 3) ? (Math.random() * 100 <= 1) : false;
                 if(fireWorker.pos.y < this.canvas.height / 5 || fireWorker.vel.y >= 0 || distance < 50 || randomChance){
+                    fireWorker.state = 1;
                     fireWorker.explode();
                 }
             }
+            
             
         },
         exist: function(){
 
         },
-        add : function(x, y){
-    
+        add : function(_this, x, y){
+            if(x === undefined || y === undefined){
+                if(_this.fireWorkers.length < _this.MAX_FIREWORKERS){
+                    var fireWorker = new FireWorker({
+                        'canvas' : _this.canvas
+                    });
+                    fireWorker.explosionColor = Math.floor(Math.random() * 360 / 10) * 10;
+                    fireWorker.vel.y = Math.random() * -3 - 4;
+                    fireWorker.vel.x = Math.random() * 6 - 3;
+                    fireWorker.size = 8;
+                    fireWorker.shrink = 0.999;
+                    fireWorker.gravity = 0.01;
+                    _this.fireWorkers.push(fireWorker);
+                }
+            }else{
+                //点击事件触发后，根据坐标计算初始速度
+            }
+            
         },
         clear : function(){
             this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);  
@@ -101,15 +113,16 @@
 
     function FireWorker(props){
         // `var pos = new Position(this.canvas.width / 2, this.canvas.height);`
-        this.pos = props.pos ? props.pos: new Position(0,0);
-        this.Particles = [];
+        this.canvas = props.canvas;
+        this.pos = props.pos ? props.pos: new Position(this.canvas.width / 2, this.canvas.height);
+        this.particles = [];
         this.MAX_PARTICLES = 400;
         this.canvas = props.canvas;
         //type 0 爆炸烟花, type 1 喷泉烟花
         this.type = props.type ? props.type: 0;
 
         //速度
-        this.vel = {
+        this.vel = props.vel ? props.vel: {
             x: 0,
             y: 0
         };
@@ -130,36 +143,70 @@
 
         //烟花Particle的数量
         this.size = props.size? props.size: Math.floor(Math.random() * 5 + 5);
+
+        //判断烟花状态的方法
+        //爆炸烟花 0 上升阶段 ,1 爆炸阶段, 2 消失(释放)
+        //喷泉烟花 0 上升阶段,            2 消失(释放)
+        this.state = 0;
+
+        var pos = new Position(this.pos.x, this.pos.y);
+        var p = new Particle({
+            'pos': pos,
+            'canvas' : this.canvas ,
+            'color' : this.color
+        });
+        p.size = 8;
+        p.vel= this.vel;
+        this.particles.push(p);
     }
 
     FireWorker.prototype = {
         render : function(ctx){
-            var pos = new Position(this.canvas.width / 2, this.canvas.height);
-            var p = new Particle({
-                'pos': pos,
-                'canvas' : this.canvas ,
-                'color' : this.color
-            });
-            p.size = 8;
-            if(p.exist()){
-                p.render(ctx);
+            for(var p of this.particles){
+                if(p.exist()){
+                    p.render(ctx);
+                }
             }
         },
         //烟花爆炸的方法
         explode : function(){
+            var count = Math.random() * 10 + 80;
 
-        },
-        //判断烟花状态的方法
-        //爆炸烟花 0 上升阶段 ,1 爆炸阶段, 2 消失(释放)
-        //喷泉烟花 0 上升阶段,            2 消失(释放)
-        state : function(){
-            return 0;
+            for (var i = 0; i < count; i++) {
+                var particle = new Particle(this.pos);
+                var angle = Math.random() * Math.PI * 2;
+    
+                // emulate 3D effect by using cosine and put more particles in the middle
+                var speed = Math.cos(Math.random() * Math.PI / 2) * 15;
+    
+                particle.vel.x = Math.cos(angle) * speed;
+                particle.vel.y = Math.sin(angle) * speed;
+    
+                particle.size = 10;
+    
+                particle.gravity = 0.2;
+                particle.resistance = 0.92;
+                particle.shrink = Math.random() * 0.05 + 0.93;
+    
+                particle.flick = true;
+                particle.color = this.explosionColor;
+    
+                this.particles.push(particle);
+            }
         },
         update : function(){
-
+            var isMissing = false;
+            for(var p of this.particles){
+                p.update();
+                if(!p.exist()){
+                    isMissing = true;
+                }
+            }
+            
+            if(isMissing){
+                this.state = 2;
+            }
         }
-
-
     }
 
 
@@ -218,6 +265,10 @@
         render : function(ctx){
             console.log('render Particle:');
             var gradient = ctx.createRadialGradient(this.pos.x, this.pos.y, 0.1, this.pos.x, this.pos.y, this.size / 2);
+            
+            // console.log('position(x:' + this.pos.x + ',y:' + this.pos.y + ')');
+            // console.log('vel(x:' + this.vel.x + ',y:' + this.vel.y + ')');
+            
             gradient.addColorStop(0.1, "rgba(255,255,255," + this.alpha + ")");
             gradient.addColorStop(0.8, "hsla(" + this.color + ", 100%, 50%, " + this.alpha + ")");
             gradient.addColorStop(1, "hsla(" + this.color + ", 100%, 50%, 0.1)");
@@ -235,7 +286,7 @@
             ctx.fill();
             ctx.restore();
         },
-        //粒子消失的方法
+        //判断粒子消失的方法
         exist : function(){
             return this.alpha >= 0.1 && this.size >= 1;
         }
